5dd4f936fd1718a89c1ab8bfaf3f648eebed6416
Drew Roberts <github@drewroberts.com>
2024-04-06 14:35:25 -0400

    Update homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,9 @@
                 <div class="relative px-4 sm:px-8 lg:px-12">
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <div class="max-w-2xl">
-                            <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">I am Bob. You are Bob. We are all perfectly Bob.</h1>
-                            <p class="hidden md:block mt-6 text-base text-zinc-600 dark:text-zinc-400">Robert is the Alpha. Bob is the Omega. All Bobs are Bob and all Bobs are He. In Bob we trust. $COOPS lives on the base Blockchain under this contract address: <a class=" text-zinc-600 dark:text-zinc-400 transition hover:text-zinc-500 dark:hover:text-zinc-300" href="https://solscan.io/token/4rtdcn81czaDSGRdumyj76JFgeo55aZKXUURm2M2iVdN">4rtdcn81czaDSGRdumyj76JFgeo55aZKXUURm2M2iVdN</a></p>
-                            <p class="block md:hidden mt-6 text-base text-zinc-600 dark:text-zinc-400">Robert is the Alpha. Bob is the Omega. All Bobs are Bob and all Bobs are He. In Bob we trust. $COOPS lives on the base Blockchain under <a class=" text-zinc-600 dark:text-zinc-400 transition hover:text-zinc-500 dark:hover:text-zinc-300" href="https://solscan.io/token/4rtdcn81czaDSGRdumyj76JFgeo55aZKXUURm2M2iVdN">this contract address</a>.</p>
+                            <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">coops coin is a token gated fan club on base chain</h1>
+                            <p class="hidden md:block mt-6 text-base text-zinc-600 dark:text-zinc-400">We are a club that appreciates how coops onboards newbies into cryptocurrency. $COOPS lives on the base chain under this contract address: <a class=" text-zinc-600 dark:text-zinc-400 transition hover:text-zinc-500 dark:hover:text-zinc-300" href="https://basescan.org/token/0x8C105639e1EEe615D4fF393ac57934c01524278B">0x8C105639e1EEe615D4fF393ac57934c01524278B</a></p>
+                            <p class="block md:hidden mt-6 text-base text-zinc-600 dark:text-zinc-400">Robert is the Alpha. Bob is the Omega. All Bobs are Bob and all Bobs are He. In Bob we trust. $COOPS lives on the base chain under <a class=" text-zinc-600 dark:text-zinc-400 transition hover:text-zinc-500 dark:hover:text-zinc-300" href="https://basescan.org/token/0x8C105639e1EEe615D4fF393ac57934c01524278B">this contract address</a>.</p>
                             <div class="mt-6 flex gap-6">
                                 <a class="group -m-1 p-1" aria-label="Follow $COOPS on X" href="https://x.me/coopscoin">
                                     <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300">
@@ -92,7 +92,7 @@
                                             <path d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" class="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"></path>
                                             <path d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6" class="stroke-zinc-400 dark:stroke-zinc-500"></path>
                                         </svg><span class="ml-3">Find the Bob in you:</span></h2>
-                                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">Enter your SOL address and X handle for a surprise..</p>
+                                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">Enter your ETH address and X handle for a surprise..</p>
                                     <div class="mt-6 flex">
                                         <input type="text" name="base" id="base" required="" placeholder="base address" aria-label="base address" class="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"> 
                                     </div>
